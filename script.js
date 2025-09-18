67768ce2fc1c77b9e07877a5af4f217ea8e642fb
FreddieAlvarez <freddie.alvarez18@gmail.com>
2025-09-18 14:00:24 -0500

    Added array filtering

--- script.js
+++ script.js
@@ -42,13 +42,26 @@ printFoodRecommendation("sushi");
 
 
 // Here's a list of 50 friends' favorite foods:
+let friendFavorites = [
+    "Pizza", "Sushi", "Pasta", "Falafel", "Burgers", "Ramen", "Pad Thai", "Curry", "Pho", "Nachos", "Gnocchi", "Donuts", "Steak", "Lasagna", "Biryani", "Tacos", "Croissant", "Churros", "Fried Rice", "Shawarma", "Miso Soup", "BBQ Ribs", "Hotpot", "Enchiladas", "Baklava", "Gyros", "Hummus", "Empanadas", "Pancakes", "Muffins", "Samosas", "Macarons", "Quiche", "Pierogi", "Arepas", "Okonomiyaki", "Ceviche", "Brisket", "Bao Buns", "Poutine", "Clam Chowder", "Fajitas", "Canelé", "Kimchi", "Tamales", "Omelette", "Biscuits", "Tempura", "Spring Rolls", "Crepes"
+  ];
 
 // 5. Print out only foods that have an "a" in the name. For example, "Pizza" would not be included, but "Pasta" would be.
-
+for (let i =0; i < friendFavorites.length; i++) {
+  if (friendFavorites[i].toLowerCase().includes("a")) {
+    console.log(friendFavorites[i]);
+  }
+}
 
 // 6. Store the result in an array called foodsWithA. Print out the array.
+let foodsWithA = [];
+for (let i = 0; i < friendFavorites.length; i++) {
+  if (friendFavorites[i].toLowerCase().includes("a")) {
+    foodsWithA.push(friendFavorites[i]);
+  }
+}
 
-
+console.log(foodsWithA);
 
 
 // 7. Create a new array longFoodNames for foods with names longer than 6 characters.
